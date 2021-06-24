38adec0f7d7fdfcefbfe3950b6d5529ba3db9275
Sol Irvine <sol@zenzen.io>
2021-06-24 13:03:34 +0900

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { IntercomProvider } from 'react-use-intercom';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <IntercomProvider appId={process.env.NEXT_PUBLIC_INTERCOM_APP_ID} autoBoot>
+      <Component {...pageProps} />
+    </IntercomProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useIntercom } from 'react-use-intercom';
+
 
 export default function Home() {
+  const { boot, shutdown, hide, show, update } = useIntercom();
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +23,9 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <button onClick={boot}>Boot intercom! ☎️</button>
+        <button onClick={show}>Show intercom! ☎️</button>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
